9aaf9fe7ea4dc3197277326a44f5462cc846908a
Luis Felipe de Azevedo Melo <felipeluis610@gmail.com>
2022-10-24 21:08:44 -0300

    Iniciando o projeto

---
+++ .slicemachine/assets/slices/Po/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Po';
+
+export default {
+  title: 'slices/Po'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Promised","spans":[]}],"description":[{"type":"paragraph","text":"Mollit id occaecat in pariatur deserunt.","spans":[]}]},"slice_type":"po","id":"_Default"}} />
+_Default.storyName = ''
